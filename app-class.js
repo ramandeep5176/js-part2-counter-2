3e9322808243ab2578d806389139e2f69172acc3
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-08-27 17:46:35 -0400

    done

---
+++ app-class.js
@@ -0,0 +1,43 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `please check "${selection}" selector, no such element exists`
+  );
+}
+
+class counter {
+  constructor(element, value) {
+    this.counter = element;
+    this.value = value;
+    this.resetBtn = element.querySelector(".reset");
+    this.increaseBtn = element.querySelector(".increase");
+    this.decreaseBtn = element.querySelector(".decrease");
+    this.valueDOM = element.querySelector(".value");
+    this.valueDOM.textContent = this.value;
+    // bind this to all functions
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.reset = this.reset.bind(this);
+    this.increaseBtn.addEventListener("click", this.increase);
+    this.decreaseBtn.addEventListener("click", this.decrease);
+    this.resetBtn.addEventListener("click", this.reset);
+  }
+  increase() {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+  }
+  decrease() {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+  }
+  reset() {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+  }
+}
+
+const firstcounter = new counter(getElement(".first-counter"), 100);
+const secondcounter = new counter(getElement(".second-counter"), 500);
